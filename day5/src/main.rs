a65a5be26780693a0aa3face9070951bd0f1986d
Jean Poizat <jean.poizat@se.com>
2024-12-07 08:45:53 +0100

    day5 part2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use regex::Regex;
 use std::env;
 use std::fs::File;
@@ -12,25 +11,74 @@ fn main() {
     let mut sum2: i32 = 0;
     let r_orders = Regex::new(r"^([0-9]+)\|([0-9]+)$").unwrap();
     let r_prints = Regex::new(r"^([0-9]+,)+[0-9]+$").unwrap();
-    // parse using filename
+    let mut orders: Vec<String> = Vec::new();
+    // parse using filename - part1
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines.flatten() {
             let result = r_orders.captures(&line);
             if result.is_some() {
                 let result = result.unwrap();
-                let key = result.get(1).unwrap().as_str();
-                let value = result.get(2).unwrap().as_str();
-                println!("key {}, value {}",key, value);
+                orders.push(result.get(0).unwrap().as_str().to_string());
                 continue;
-                }
+            }
+            // if we are there we are done with the constraints so orders is full
+            // now we tackle the lines
             let result = r_prints.captures(&line);
             if result.is_some() {
-                println!("prints {}", result.unwrap().get(0).unwrap().as_str());
-
+                let prints: Vec<_> = result
+                    .unwrap()
+                    .get(0)
+                    .unwrap()
+                    .as_str()
+                    .split(',')
+                    .map(String::from)
+                    .collect();
+                // build all pairs
+                if is_ok(prints.clone(), orders.clone()) {
+                    sum += prints[prints.len() / 2].parse::<i32>().unwrap()
+                }
             }
         }
     }
+    // part2
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.flatten() {
+            let result = r_orders.captures(&line);
+            if result.is_some() {
+                let result = result.unwrap();
+                orders.push(result.get(0).unwrap().as_str().to_string());
+                continue;
+            }
+            // if we are there we are done with the constraints so orders is full
+            // now we tackle the lines
+            let result = r_prints.captures(&line);
+            if result.is_some() {
+                let mut prints: Vec<_> = result
+                    .unwrap()
+                    .get(0)
+                    .unwrap()
+                    .as_str()
+                    .split(',')
+                    .map(String::from)
+                    .collect();
+                // we take now the nok prints and rework them
+                if !is_ok(prints.clone(), orders.clone()) {
+                    // we build the order from scratch
+                    let mut prints_splice: Vec<String> = vec![prints[0].clone()];
+                    prints.remove(0);
+                    for page in prints.iter() {
+                        prints_splice = order(page.to_string(), prints_splice.clone(), orders.clone());
+                    }
+                    
+                    sum2 += prints_splice[prints_splice.len() / 2].parse::<i32>().unwrap()
+                }
+            }
+        }
+    }
+    println!("sum is {}", sum);
+    println!("sum2 is {}", sum2);
 }
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
@@ -42,3 +90,35 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn is_ok(prints: Vec<String>, orders: Vec<String>) -> bool {
+    let mut ok: bool = true;
+    for (num, page) in prints.iter().enumerate() {
+        for otherpage in prints[num + 1..].iter() {
+            let test_order = page.to_string() + "|" + otherpage;
+            ok = ok && orders.iter().any(|i| *i == test_order);
+        }
+        if num > 0 {
+            for otherpage in prints[..num].iter() {
+                let test_order = page.to_string() + "|" + otherpage;
+                ok = ok && !orders.iter().any(|i| *i == test_order);
+            }
+        }
+    }
+    return ok;
+}
+
+fn order(item: String, prints: Vec<String>, orders: Vec<String>) -> Vec<String> {
+    //insert an item into a prints
+    //dbg!("calling order with item: {}, prints {} ", &item, &prints);
+    for i in 0..prints.len() + 1 {
+        let mut new_prints = prints.clone();
+        new_prints.insert(i, item.clone());
+        // dbg!("prints after insert {}", &new_prints);
+        if is_ok(new_prints.clone(), orders.clone()) {
+            // dbg!("prints is ok {}", &new_prints);
+            return new_prints;
+        }
+    }
+    println!("raté ");
+    return vec!["".to_string()];
+}
